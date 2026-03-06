47c80f1cba8c2e925264b624fa457409f995f958
Ana Joandrea <anamaria.joandrea@ulbsibiu.ro>
2026-03-06 09:34:02 +0200

    Initial Setup

---
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -0,0 +1,34 @@
+package ro.ulbs.proiectaresoftware.students;
+
+public class Student {
+    int numarMatricol;
+    String prenume;
+    String nume;
+    String formatieDeStudiu;
+
+    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
+    public void getDate(){
+        System.out.println(getNumarMatricol() + " " + getNume() + ' ' + getPrenume() + ' ' + getFormatieDeStudiu());
+    }
+}
