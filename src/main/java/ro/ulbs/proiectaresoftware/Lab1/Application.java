344f599f99b9c9976aec34d6310c6e5668d835d2
Ana Joandrea <anamaria.joandrea@ulbsibiu.ro>
2026-06-01 21:15:14 +0300

    lab1

--- src/main/java/ro/ulbs/proiectaresoftware/Lab1/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/Lab1/Application.java
@@ -1,7 +1,35 @@
 package ro.ulbs.proiectaresoftware.Lab1;
 
 public class Application {
-    static void main() {
+    public static void main(String[] args) {
 
+        //b)
+        String alfabet = "";
+        for (char c = 'a'; c <= 'z'; c++) {
+            alfabet = alfabet + c;
+        }
+
+        System.out.println("Litere mici: " + alfabet.toLowerCase());
+        System.out.println("Majuscule: " + alfabet.toUpperCase());
+
+        //c)
+        String[] array = new String[5];
+        int indexArray = 0;
+        array[0] = "";
+
+        for (int i = 0; i < alfabet.length(); i++) {
+            char c = alfabet.charAt(i);
+            boolean esteVocala = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+            if (esteVocala && i != 0) {
+                indexArray++;
+                array[indexArray] = "";
+            }
+            array[indexArray] = array[indexArray] + c;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("array[" + i + "] = \"" + array[i] + "\"");
+        }
     }
 }
